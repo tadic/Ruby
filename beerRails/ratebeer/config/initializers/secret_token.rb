43267e110e794189c7cf2d41e4f10633f4703077
tadic <ivantadic014@gmail.com>
2014-01-10 23:04:01 +0200

    first commit

---
+++ beerRails/ratebeer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ratebeer::Application.config.secret_token = '8a1c8071e18f3ad9071791d3410e2810dba49355be002f459ef88c28f59944a9ee95754fffd9fe9dcaf8f6df1f37fb78260ebcff2d0898c45db3d5b7a427dcdc'
