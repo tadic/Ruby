ff29a94593aaf8e3385c752d5ab2ea5c1cbdd457
tadic <ivantadic014@gmail.com>
2014-02-01 23:11:33 +0200

    4

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140131201641) do
+ActiveRecord::Schema.define(version: 20140201210957) do
 
   create_table "beer_clubs", force: true do |t|
     t.string   "name"
