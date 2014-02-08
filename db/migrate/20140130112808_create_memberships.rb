7d2c1f5119fe96349271fab91ff5c10ab4d32a63
tadic <ivantadic014@gmail.com>
2014-02-08 23:06:30 +0200

    travis123

--- db/migrate/20140130112808_create_memberships.rb
+++ db/migrate/20140130112808_create_memberships.rb
@@ -2,7 +2,7 @@ class CreateMemberships < ActiveRecord::Migration
   def change
     create_table :memberships do |t|
       t.integer :beer_club_id
-      t.integer :user_id
+#      t.integer :user_id
 
       t.timestamps
     end
